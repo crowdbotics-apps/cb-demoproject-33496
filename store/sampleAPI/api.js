8962cad7532ef0895db9a5566d2c0e28dc3a0273
Crowdbotics <team@crowdbotics.com>
2022-02-11 18:46:46 +0000

    Update store.

--- store/sampleAPI/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-const sampleAPI = axios.create({
-  baseURL: "https://reqres.in/api/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
